754c2916b59a7d4a878fe8152a0f5f5938519e1d
Master J <jake.yanov@gmail.com>
2024-07-04 01:35:15 +0300

    Configure deployment to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+output: 'export',
   assetPrefix: isProd ? '/grid/' : '',
   basePath: isProd ? '/grid' : '',
   trailingSlash: true,
